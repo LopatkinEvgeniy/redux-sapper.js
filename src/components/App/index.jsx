8437fb0b68dc7080553785a7f2287ca2abbf4db2
evgen <javany25@mail.ru>
2016-04-18 22:11:57 +0300

    Change controlls html & scss

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,8 +7,9 @@ export default () => (
       <h1 className="app__title">Sapper.js</h1>
     </header>
     <div className="app__content">
-
-      <div className="app_controlls"></div>
+      <div className="app_controlls">
+        <button className="button button_raised_blue">new</button>
+      </div>
 
       <div className="app__field">
         <Field />
