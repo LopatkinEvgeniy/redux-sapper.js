a9cabb469043a7e42e026c47986471c234d1f5a9
evgen <javany25@mail.ru>
2016-04-18 21:56:10 +0300

    Function generateRows refactor

--- src/utils/generateRows.js
+++ src/utils/generateRows.js
@@ -26,46 +26,51 @@ export default function generateRows({ rowsCount, colsCount, bombsFactor }) {
 
   // calculate bombs around each empty cell
   rows = rows.map((row, rowIndex) => row.map((cell, colIndex) => {
+    const hasTop = rowIndex > 0;
+    const hasBottom = rowIndex < (rowsCount - 1);
+    const hasLeft = colIndex > 0;
+    const hasRight = colIndex < (colsCount - 1);
+
     // increment bomb count to each neighboring cell
     if (cell.hasBomb) {
       // top cell
-      if (rowIndex < (rowsCount - 1)) {
-        rows[rowIndex + 1][colIndex].bombsAroundCount++;
-      }
-
-      // bottom cell
-      if (rowIndex > 0) {
+      if (hasTop) {
         rows[rowIndex - 1][colIndex].bombsAroundCount++;
       }
 
+      // bottom cell
+      if (hasBottom) {
+        rows[rowIndex + 1][colIndex].bombsAroundCount++;
+      }
+
       // left cell
-      if (colIndex > 0) {
+      if (hasLeft) {
         rows[rowIndex][colIndex - 1].bombsAroundCount++;
       }
 
       // right cell
-      if (colIndex < (colsCount - 1)) {
+      if (hasRight) {
         rows[rowIndex][colIndex + 1].bombsAroundCount++;
       }
 
       // top-left cell
-      if ((rowIndex < (rowsCount - 1)) && (colIndex > 0)) {
-        rows[rowIndex + 1][colIndex - 1].bombsAroundCount++;
-      }
-
-      // top-right
-      if ((rowIndex < (rowsCount - 1)) && (colIndex < (colsCount - 1))) {
-        rows[rowIndex + 1][colIndex + 1].bombsAroundCount++;
-      }
-
-      // bottom-left
-      if ((rowIndex > 0) && (colIndex > 0)) {
+      if (hasTop && hasLeft) {
         rows[rowIndex - 1][colIndex - 1].bombsAroundCount++;
       }
 
-      // bottom-right
-      if ((rowIndex > 0) && (colIndex < (colsCount - 1))) {
+      // top-right
+      if (hasTop && hasRight) {
         rows[rowIndex - 1][colIndex + 1].bombsAroundCount++;
+      }
+
+      // bottom-left
+      if (hasBottom && hasLeft) {
+        rows[rowIndex + 1][colIndex - 1].bombsAroundCount++;
+      }
+
+      // bottom-right
+      if (hasBottom && hasRight) {
+        rows[rowIndex + 1][colIndex + 1].bombsAroundCount++;
       }
     }
 
